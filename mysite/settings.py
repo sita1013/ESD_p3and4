2037b729ca59ccf9af9ccf26f5693e9cbb749139
sita1013 <seluvaiasariahita@gmail.com>
2025-03-24 15:30:20 +0000

    adding staticfiles for django to settins

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
