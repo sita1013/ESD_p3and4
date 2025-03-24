104135095353aaa11003b3e1f87787d0bff0d642
sita1013 <seluvaiasariahita@gmail.com>
2025-03-24 16:02:20 +0000

    adding staticfiles for django to settins

--- temperature_stories/apps.py
+++ temperature_stories/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
-class TemperatureStoriesConfig(AppConfig):
+class EsdHomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'temperature_stories'
